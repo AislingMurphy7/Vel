b6ad48c21d6e2ea33090799953f50926b28231e8
AislingMurphy7 <aislingmurphy7@gmail.com>
2018-04-10 20:48:48 +0100

    Graph database

--- app/src/main/java/com/example/user/vel/Graphview.java
+++ app/src/main/java/com/example/user/vel/Graphview.java
@@ -3,7 +3,6 @@
  */
 package com.example.user.vel;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,31 +10,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class Graphview extends AppCompatActivity
 {
-    //Holds the values gathered from firebase
-    DatabaseReference databaseReference;
-    ArrayAdapter<String> arrayAdapter;
-    ListView listViewG;
-    VW_Data info;
-    List<String> graphlist;
+
+    //Declare LineGrapheSeries of type Datapoint
+    LineGraphSeries<DataPoint> series;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -43,45 +35,54 @@ public class Graphview extends AppCompatActivity
         setContentView(R.layout.activity_graphview);
 
         //XML variable
-        listViewG = findViewById(R.id.listviewG);
+        ListView listView2 = findViewById(R.id.listviewG);
 
-        graphlist = new ArrayList<>();
+        /*Database variable is getting the connection to the firebase database via google-services
+        JSON file and making reference to the child of "Make"*/
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("VW_Data").child("0");
 
-        databaseReference= FirebaseDatabase.getInstance().getReference("VW_Data").child("0");
+        //Holds the valuse gathered from firebase
+        final ArrayList<String> carlist = new ArrayList<>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.itemview, carlist);
+        //Set the ArrayAdapter to the listview
+        listView2.setAdapter(arrayAdapter);
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
-                {
-                    info = dataSnapshot1.getValue(VW_Data.class);
-                    graphlist.add(info.Bearing);
-                }
-                arrayAdapter = new ArrayAdapter<>(Graphview.this, android.R.layout.simple_list_item_1, graphlist);
-            }
+        //ChildEventListener allows child events to be listened for
+        database.addChildEventListener(new ChildEventListener()
+        {
+            //Will run when the app is started and when there is data added to the database
+            public void onChildAdded(DataSnapshot dataSnapshot, String s)
+            {
+                //Holds the Datasnapshot value of the database as type String
+                String value = dataSnapshot.getValue(String.class);
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-
-
-
-
-
-               /* String bearing = dataSnapshot.getValue(String.class);
                 //Add the info retrieved from datasnapshot into the ArrayList
-                graphlist.add(bearing);
-                arrayAdapter = new ArrayAdapter<>(Graphview.this, android.R.layout.simple_list_item_1, graphlist);
-                listViewG.setAdapter(arrayAdapter);
-                //Will refresh app when the data changes in the database */
+                carlist.add(value);
+                //Will refresh app when the data changes in the database
+                arrayAdapter.notifyDataSetChanged();
+            }//End onChildAdded()
+            //Will run when data within the database is changed/edited
+            public void onChildChanged(DataSnapshot dataSnapshot, String s)
+            {
 
+            }//End onChildChanged()
+            //Will run when data within the database is removed
+            public void onChildRemoved(DataSnapshot dataSnapshot)
+            {
 
+            }//End onChildRemoved()
+            //Will run when data within the database is moved to different location
+            public void onChildMoved(DataSnapshot dataSnapshot, String s)
+            {
 
+            }//End onChildMoved()
+            //Will run when any sort of error occurs
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            }//End onCancelled()
+
+        });
     }//End onCreate()
 
     //Function creates the dropdown toolbar menu
