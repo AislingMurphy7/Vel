fa627a9e14ba6c71eb1503ed096c6bf3b295f194
AislingMurphy7 <aislingmurphy7@gmail.com>
2018-07-02 12:26:07 +0100

    Code comments added

--- app/src/main/java/com/example/user/vel/LoginUser.java
+++ app/src/main/java/com/example/user/vel/LoginUser.java
@@ -23,94 +23,133 @@ import com.google.firebase.auth.FirebaseAuth;
     the user to the sign up / register page.
  */
 
-public class LoginUser extends Activity implements View.OnClickListener{
-
+public class LoginUser extends Activity implements View.OnClickListener
+{
+    //Declare FireBase object
     FirebaseAuth mAuth;
+
+    //Define variables
     EditText editTextEmail, editTextPassword;
     ProgressBar progressbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        //Removes actionbar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Sets the layout according to the XML file
         setContentView(R.layout.activity_login_user);
 
+        //Instantiate mAuth
         mAuth = FirebaseAuth.getInstance();
 
+        //XML variables
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         progressbar = findViewById(R.id.progressbar);
 
+        //Attach OnClickListener to TextView
         findViewById(R.id.textViewSignup).setOnClickListener(this);
+        //Attach onClickListener to login button
         findViewById(R.id.buttonLogin).setOnClickListener(this);
-    }
+    }//End onCreate()
 
+    //Validate the user information
     private void userLogin()
     {
+        //Gather the input information
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        if(email.isEmpty()) {
+        //If the email EditText is empty
+        if(email.isEmpty())
+        {
+            //An error message is displayed
             editTextEmail.setError(getText(R.string.email_need));
+            //Show error
             editTextEmail.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the email entered does not match the layout of an email (eg, a@gmail.com)
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
         {
+            //An error message is displayed
             editTextEmail.setError(getText(R.string.ent_valid_email));
+            //Show error
             editTextEmail.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the password EditText is empty
         if(password.isEmpty())
         {
+            //An error message is displayed
             editTextPassword.setError(getText(R.string.pass_empt));
+            //Show error
             editTextPassword.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the password length is less than six
         if(password.length()<6)
         {
+            //An error message is displayed
             editTextPassword.setError(getText(R.string.mini_length));
+            //Show error
             editTextPassword.requestFocus();
             return;
-        }
+        }//End if()
 
+        //Sets the progressbar to visible on screen
         progressbar.setVisibility(View.VISIBLE);
 
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                progressbar.setVisibility(View.GONE);
-                if(task.isSuccessful()){
-                    finish();
-                    Intent intent = new Intent(LoginUser.this, userProfile.class);
-                    startActivity(intent);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                }else {
-                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+        //Logging user in viz inforaation storred in FireBase
+        mAuth.signInWithEmailAndPassword(email, password)
+                //Detects the completion
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        //Sets the progressbar to invisible on screen
+                        progressbar.setVisibility(View.GONE);
+                        //If login in was successful
+                        if(task.isSuccessful())
+                        {
+                            //Clear previous pages
+                            finish();
+                            //Login success, update UI with the signed-in user's information
+                            Intent intent = new Intent(LoginUser.this, userProfile.class);
+                            startActivity(intent);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        }//End if()
+                        else
+                            {
+                                //Appropriate error message is displayed
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }//End else()
+                    }//End onComplete()
+                });//End addOnCompleteListener
+        }//End userLogin()
 
     @Override
     public void onClick(View v)
     {
         switch (v.getId())
         {
+            /*If sign up text is selected the user will get redirected to the sign up
+             screen using an Intent statement*/
             case R.id.textViewSignup:
-
                 finish();
                 startActivity(new Intent(this, SignupUser.class));
-
                 break;
 
+            //If user taps login button, userLogin function will be ran
             case  R.id.buttonLogin:
                 userLogin();
                 break;
-        }
-
-    }
-}
+        }//End switch()
+    }//End onClick()
+}//End LoginUser()
--- app/src/main/java/com/example/user/vel/SignupUser.java
+++ app/src/main/java/com/example/user/vel/SignupUser.java
@@ -24,104 +24,144 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
     the user to the login page if they already have an account created.
  */
 
-public class SignupUser extends Activity implements View.OnClickListener {
-
+public class SignupUser extends Activity implements View.OnClickListener
+{
+    //Declare FireBase object
     private FirebaseAuth mAuth;
+
+    //Define variables
     ProgressBar progressbar;
     EditText editTextEmail, editTextPassword;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        //Removes actionbar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Sets the layout according to the XML file
         setContentView(R.layout.activity_signup_user);
 
+        //XML variables
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         progressbar = findViewById(R.id.progressbar);
 
+        //Instantiate mAuth
         mAuth = FirebaseAuth.getInstance();
 
+        //Attach onClickListener to sign up button
         findViewById(R.id.buttonSignup).setOnClickListener(this);
+        //Attach OnClickListener to TextView
         findViewById(R.id.textViewLogin).setOnClickListener(this);
-    }
+    }//End OnCreate()
 
+    //Validate the user information
     private void registerUser()
     {
+        //Gather the input information
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        if(email.isEmpty()) {
+        //If the email EditText is empty
+        if(email.isEmpty())
+        {
+            //An error message is displayed
             editTextEmail.setError(getText(R.string.email_need));
+            //Show error
             editTextEmail.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the email entered does not match the layout of an email (eg, a@gmail.com)
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
         {
+            //An error message is displayed
             editTextEmail.setError(getText(R.string.ent_valid_email));
+            //Show error
             editTextEmail.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the password EditText is empty
         if(password.isEmpty())
         {
+            //An error message is displayed
             editTextPassword.setError(getText(R.string.pass_empt));
+            //Show error
             editTextPassword.requestFocus();
             return;
-        }
+        }//End if()
 
+        //If the password length is less than six
         if(password.length()<6)
         {
+            //An error message is displayed
             editTextPassword.setError(getText(R.string.mini_length));
+            //Show error
             editTextPassword.requestFocus();
             return;
-        }
+        }//End if()
 
+        //Sets the progressbar to visible on screen
         progressbar.setVisibility(View.VISIBLE);
 
+        //Creating the user in FireBase
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                //Detects the completion
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        //Sets the progressbar to invisible on screen
                         progressbar.setVisibility(View.GONE);
+                        //If user creation was successful
                         if(task.isSuccessful())
                         {
+                            //Clear previous pages
                             finish();
                             //Sign in success, update UI with the signed-in user's information
                             Toast.makeText(getApplicationContext(), R.string.reg_user, Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(SignupUser.this, userProfile.class));
 
-                        }else {
-                            if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                        }//End if()
+                        else
                             {
-                                Toast.makeText(getApplicationContext(), R.string.reg_already, Toast.LENGTH_SHORT).show();
+                                //If the user already exists within FireBase
+                                if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                                {
+                                    //Message is displayed
+                                    Toast.makeText(getApplicationContext(), R.string.reg_already, Toast.LENGTH_SHORT).show();
 
-                            }else{
-                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
-
-
-    }
+                                }//End if()
+                                //If there is another reason
+                                else
+                                    {
+                                        //Appropriate error message is displayed
+                                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }//End else()
+                        }//End else()
+                    }//End onComplete()
+                });//End addOnCompleteListener
+    }//End registerUser()
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            //If user taps sign up button, Register function will be ran
             case R.id.buttonSignup:
                 registerUser();
                 break;
 
+            /*If login text is selected the user will get redirected to the Login screen
+             using an Intent statement*/
             case R.id.textViewLogin:
                 finish();
                 startActivity(new Intent(this, LoginUser.class));
                 break;
-
-
-        }
-
-    }
-
-}
+        }//End switch()
+    }//End onClick()
+}//End SignupUser()
